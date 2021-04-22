a9bdb85819c7c52ee29edc63943487d629b1d3c8
Patrick Tung <tungpatrick@gmail.com>
2021-04-22 17:40:52 +1000

    Set spacing and title

--- load_NC_files.py
+++ load_NC_files.py
@@ -1,18 +1,22 @@
 # %%
 from netCDF4 import Dataset
 
-rootgrp = Dataset(r"C:\Users\Patrick\Music\tomo_R_LoRes.nc", "r")
+rootgrp = Dataset(r"C:\Users\Patrick\OneDrive - UNSW\Desktop\1_CTDev\CTDev_Lakshmi_Civil\CIVIL_Steve_sample2by1_43mm_bot_SF_BeamC\tomo_RMG_LoRes.nc", "r")
 
 ds = rootgrp['tomo'][:]
-print("Voxel size is ",rootgrp.voxel_size_xyz[0]*1000, " microns")
+# print("Voxel size is ",rootgrp.voxel_size_xyz[0]*1000, " microns")
 # print(rootgrp.total_grid_size_xyz)
 
-rootgrp.close()
 
 # %%
 from ORSModel import createChannelFromNumpyArray
 labels_channel = createChannelFromNumpyArray(ds)
-labels_channel.setTitle('Labels')
+labels_channel.setTitle(rootgrp.dataset_id)
+labels_channel.setXSpacing(rootgrp.voxel_size_xyz[0]/1000)
+labels_channel.setYSpacing(rootgrp.voxel_size_xyz[1]/1000)
+labels_channel.setZSpacing(rootgrp.voxel_size_xyz[2]/1000)
 # Set voxel size
 
 labels_channel.publish()
+
+rootgrp.close()
