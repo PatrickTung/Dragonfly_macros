c4ae644e81d4ee2df2c27f743284e7f9c0ccf08a
Patrick Tung <tungpatrick@gmail.com>
2021-05-06 19:34:59 +1000

    Adjusted for more ROIs

--- load_separate_rois_as_dataset.py
+++ load_separate_rois_as_dataset.py
@@ -33,22 +33,25 @@ array = channel.getNDArray()
 
 # %%
 #! User Input
-slice_numbers =[27,83,103,139,159,181,223,253,281,303,363,409,446]
+slice_numbers =[27,83,103,139,159,181,208,223,253,261,281,303,314,330,345,363,365,409,446]
 counter = 1
 for i in slice_numbers:
     #! User Input
     # file_prefix = r"C:\Users\Patrick\OneDrive - UNSW\CTDev_FuelCell_DL_demo\QM_segment_images\block00000000.view"
     # file_suffix = r".labels.tif"
     # file_dir = file_prefix + str(i).rjust(3,'0') + file_suffix
-    file_prefix = "C:\\Users\\Patrick\\OneDrive - UNSW\\CTDev_FuelCell_DL_demo\\fuel_cell_2D\\fuel_cell_2D\\label_convert\\"
+    # file_prefix = "C:\\Users\\Patrick\\OneDrive - UNSW\\CTDev_FuelCell_DL_demo\\fuel_cell_2D\\fuel_cell_2D\\label_convert\\"
+    file_prefix = "C:\\Users\\Patrick\\Downloads\\label_new_convert_PT\\"
 
-    file_suffix = r".tif"
+    # file_suffix = r".labels.tif"
+    file_suffix = r".png"
+    # file_dir = file_prefix + f'{i:03}' + file_suffix
     file_dir = file_prefix + str(counter) + file_suffix
-    if counter != 6:
-        print(file_dir)
-        im = Image.open(file_dir)
-        imarray = np.array(im)
-        array[i-1,:,:] = imarray[:]
+    # if counter != 6:
+    print(file_dir)
+    im = Image.open(file_dir)
+    imarray = np.array(im)
+    array[i-1,:,:] = imarray[:]
     # print(array.shape)
     # print(imarray.shape)
     counter += 1
@@ -71,8 +74,11 @@ for i in slice_numbers:
 
 from ORSModel import createChannelFromNumpyArray
 labels_channel = createChannelFromNumpyArray(array)
+labels_channel.setXSpacing(voxel_size/1000000)
+labels_channel.setYSpacing(voxel_size/1000000)
+labels_channel.setZSpacing(voxel_size/1000000)
 labels_channel.setTitle('Labels')
 labels_channel.publish()
 
 
-mask_roi.publish()
+# mask_roi.publish()
